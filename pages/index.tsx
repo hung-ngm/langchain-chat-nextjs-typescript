50f2f9e66866e5823bb51a73524deebdfb1cc9b7
minhhung123 <prohungnguyenminh@gmail.com>
2023-04-15 02:29:57 +1000

    Update github url

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,7 +134,7 @@ const Home: NextPage = () => {
         </div>
         <div className = {styles.navlinks}>
           <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Docs</a>
-          <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">GitHub</a>
+          <a href="https://github.com/minhhung123/langchain-chat-nextjs-typescript" target="_blank">GitHub</a>
         </div>
       </div>
 
